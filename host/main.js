720e7f3293fc634bfc8ac7ced997a44702e8c553
Madhushree Gupta <madhushreegupta@madhushrees-mbp.home>
2024-12-16 11:25:08 +0000

    microfront-end for header and trending

--- host/main.js
+++ host/main.js
@@ -1,7 +1,7 @@
 // main.js
-import { mount as mountHeader } from '../apps/header/dist/header.js';
-import { mount as mountTrending } from '../apps/trending/dist/trending.js';
-import { mount as mountHighlights } from '../apps/highlights/dist/highlights.js';
+import { mount as mountHeader } from './apps/header/dist/header.js';
+import { mount as mountTrending } from './apps/trending/dist/trending.js';
+import { mount as mountHighlights } from './apps/highlights/dist/highlights.js';
 
 mountHeader(document.querySelector('#header'));
 mountTrending(document.querySelector('#trending'));
