2d7edf7698e6d015214e9aac1cf32280093355e1
Madhushree Gupta <madhushreegupta@madhushrees-mbp.home>
2024-12-16 10:17:08 +0000

    Vite app setup

---
+++ host/main.js
@@ -0,0 +1,8 @@
+// main.js
+import { mount as mountHeader } from '../apps/header/dist/header.js';
+import { mount as mountTrending } from '../apps/trending/dist/trending.js';
+import { mount as mountHighlights } from '../apps/highlights/dist/highlights.js';
+
+mountHeader(document.querySelector('#header'));
+mountTrending(document.querySelector('#trending'));
+mountHighlights(document.querySelector('#highlights'));
